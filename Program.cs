b865e5afeb03b472a6712d16362bf5db337080fe
Mayank Negi (1000118247) <NEGIM3@student.op.ac.nz>
2023-05-09 13:36:55 +1200

    creating new project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Nick_Is_Our_Leader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
